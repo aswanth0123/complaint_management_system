4c7616b7a1fe5b48f5da0048af853bb45af5a432
aswanth0123 <aswanthsuresh74@gmail.com>
2025-07-28 12:44:14 +0530

    server running file  updation for Production

--- complaint_management/complaint_management/settings.py
+++ complaint_management/complaint_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+3v*kvtgs)z!dvj_^23@hwism#lul+y!(5f@-!kn+h8o&!7(zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
